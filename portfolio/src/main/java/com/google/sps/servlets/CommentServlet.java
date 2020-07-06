face46cacb0b731c11d2032576783c8f8eef02a5
chris98122 <chris98122@126.com>
2020-07-07 02:21:44 +0800

    update servlet

--- portfolio/src/main/java/com/google/sps/servlets/CommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentServlet.java
@@ -20,19 +20,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
-public class DataServlet extends HttpServlet {
-
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
-}
-
 @WebServlet("/comment")
-public final class TextProcessorServlet extends HttpServlet {
+public final class CommentServelet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
